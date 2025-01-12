61d8d80eb388fe904c6a565c7824ac9c03dc8cf3
oextraordimario <contatoextraordimario@gmail.com>
2025-01-12 11:36:42 -0300

    Altered exercercises_ref table.

--- scripts/CREATE gold_dim_exercisesref.sql
+++ scripts/CREATE gold_dim_exercisesref.sql
@@ -3,13 +3,13 @@ CREATE TABLE workouts.gold_dim_exercisesref (
 	custom_exercise BOOL,
 	exercise_name TEXT,
 	equipment TEXT,
+	muscle_group TEXT,
+	exercise_group TEXT,
 	main_muscle TEXT,
 	auxiliary_muscles TEXT,
 	size TEXT,
 	metrics TEXT,
 	bodyweight TEXT,
 	notes TEXT,
-	muscle_group TEXT,
-	exercise_group TEXT,
 	PRIMARY KEY (exercise_name, equipment)
 )
