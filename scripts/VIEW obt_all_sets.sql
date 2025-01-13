d0235eb1eeab51db89cc1940056d0a9ff0c36e83
oextraordimario <contatoextraordimario@gmail.com>
2025-01-13 15:02:15 -0300

    Created the two views.

--- scripts/VIEW obt_all_sets.sql
+++ scripts/VIEW obt_all_sets.sql
@@ -1,6 +1,8 @@
-CREATE OR REPLACE VIEW ap_workouts.obt_all_sets AS
+-- DROP VIEW ap_workouts.view_obt_allsets
+CREATE OR REPLACE VIEW ap_workouts.view_obt_allsets AS
 SELECT
 	"sets".id
+	, "sets".workout_id
 	, workouts.start_time AS "workout_start_time"
 	, workouts.end_time AS "workout_end_time"
 	, workouts.duration AS "workout_duration_min"
@@ -16,6 +18,13 @@ SELECT
 	, "sets".total_weight
 	, "sets".quantity
 	, "sets".volume
+FROM ap_workouts.gold_fact_sets AS "sets"
+LEFT JOIN ap_workouts.gold_dim_exercisesref AS "exercises"
+	ON "sets".exercise_id = exercises.id
+LEFT JOIN ap_workouts.gold_dim_workouts AS "workouts"
+	ON "sets".workout_id = workouts.id
+ORDER BY 1 ASC
+	
 	--, workouts.day_name
 	--, workouts.day_number
 	--, workouts.week_number
@@ -33,9 +42,3 @@ SELECT
 	--, exercises.metrics
 	--, exercises.bodyweight
 	--, exercises.notes
-	--, workouts.id
-FROM ap_workouts.gold_fact_sets AS "sets"
-LEFT JOIN ap_workouts.gold_dim_exercisesref AS "exercises"
-	ON "sets".exercise_id = exercises.id
-LEFT JOIN ap_workouts.gold_dim_workouts AS "workouts"
-	ON "sets".workout_id = workouts.id
